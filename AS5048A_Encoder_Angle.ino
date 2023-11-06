e94c29ad152cda552c6e3ecd5e12e6d9a138da87
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2023-11-05 19:45:06 -0500

    Create AS5048A_Encoder_Angle.ino

---
+++ AS5048A_Encoder_Angle.ino
@@ -0,0 +1,76 @@
+#include <SPI.h>
+
+/*
+Wiring between AS5048A and the Arduino Uno:
+
+5V of the AS5048A to 5V on the Arduino Uno.
+Or 
+3.3V of the AS5048A to 3.3V on the Arduino Uno.
+Which ever you prefer to use.
+GND to GND.
+MOSI (Master Out Slave In) of AS5048A to D11 on Arduino (MOSI).
+MISO (Master In Slave Out) to D12 on Arduino (MISO).
+SCK (Serial Clock) to D13 on Arduino (SCK).
+CS (Chip Select) to  D10 on Arduino.
+
+This code will read out the angle in degrees from the AS5048A.
+*/
+
+
+// Chip Select Pin for AS5048A
+const int CSPin = 10;
+
+void setup() {
+  // Start the serial communication
+  Serial.begin(115200);
+  
+  // Set up the chip select pin as an output
+  pinMode(CSPin, OUTPUT);
+  
+  // Begin SPI
+  SPI.begin();
+  
+  // Configure SPI for the AS5048A
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1)); // Adjust the speed if needed
+  
+  // Pull CS pin high to deselect the encoder
+  digitalWrite(CSPin, HIGH);
+}
+
+void loop() {
+  // Variable to hold the raw angle data
+  unsigned int angleRaw = readAS5048A();
+  
+  // Convert the raw data to degrees (0 - 360)
+  float angleDeg = (float)angleRaw * 360.0 / 16384.0;
+  
+  // Print the angle in degrees to the serial monitor
+  Serial.print("Angle: ");
+  Serial.print(angleDeg);
+  Serial.println(" degrees");
+  
+  // Small delay before reading again
+  //delay(100);
+}
+
+unsigned int readAS5048A() {
+  // Variable to store the received data
+  unsigned int data = 0;
+  
+  // Pull CS pin low to select the encoder
+  digitalWrite(CSPin, LOW);
+  
+  // Wait for the data to be ready
+  delayMicroseconds(1);
+  
+  // Send read angle command (0xFFFF) and receive 16 bits of data
+  data = SPI.transfer16(0xFFFF);
+  
+  // Pull CS pin high to deselect the encoder
+  digitalWrite(CSPin, HIGH);
+  
+  // Data processing: clear the error flag (bit 14) and the parity bit (bit 15)
+  data = data & 0x3FFF;
+  
+  return data;
+}
